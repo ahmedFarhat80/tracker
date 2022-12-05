93a63aacb2ce64f8a7021acbf14524eed055587e
mykholy <mykholy30@gmail.com>
2022-12-05 16:43:44 +0200

    =add orders created at and distance in drivers resources

--- app/Http/Resources/Admin/DriverResource.php
+++ app/Http/Resources/Admin/DriverResource.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Resources\Admin;
+use App\Models\Order;
+use Carbon\Carbon;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,9 +16,15 @@ class DriverResource extends JsonResource
      */
     public function toArray($request)
     {
-	 $array = parent::toArray($request);
+
+  	$distance        = Order::where('driver_id', '=', $this->id)->whereDate('created_at', Carbon::today())->sum('distance');
+
+	$orders_delivered=Order::where('driver_id', '=', $this->id)->where('status','=', 'delivered')->count();
+	$array = parent::toArray($request);
 	$array ['users']= $this->users;
         $array ['orders']= $this->orders;
+	$array ['distance']= $distance   ;
+	$array ['orders_delivered']=$orders_delivered;
        
         // $this->orders()->with(['driver','restaurant']);
         // $this->acceptedOrders;
