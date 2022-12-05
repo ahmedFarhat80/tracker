dae65ce6e00d260fe11c2cce0675d8f11184cca0
mykholy <mykholy30@gmail.com>
2022-12-05 16:18:36 +0200

    =fix class name address filed to drivers

--- database/migrations/2022_12_05_034531_add_address_to_drivers_table.php
+++ database/migrations/2022_12_05_034531_add_address_to_drivers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddOtpToDriversTable extends Migration
+class AddAddressToDriversTable extends Migration
 {
     /**
      * Run the migrations.
